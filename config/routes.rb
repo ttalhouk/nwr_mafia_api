f9a68bf7805f3d60df7be3d3ed7b6311e282b0f2
Talal Talhouk <talal@sbcglobal.net>
2016-08-31 20:13:36 -0700

    setup seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :games, except: :destroy
-  resources :players, only: [:new, :show, :edit, :update]
+  resources :players, only: [:new,:create, :show, :edit, :update]
+  post '/login' => 'logins#create'
+  get '/logout' => 'logins#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
